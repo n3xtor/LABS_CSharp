0574d8415ae7f90aa78aa4092af019db721e8bcd
Pavlo Kikis <99750479+n3xtor@users.noreply.github.com>
2023-04-24 22:20:47 +0300

    LAB3

---
+++ LAB3/Lab3/Book.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace BookStore
+{
+    interface IPages
+    {
+        int Pages { get; set; }
+        int Volume();
+    }
+
+    interface IBookOperations
+    {
+        Book AddBooks(Book b);
+        bool CheckEquality(Book b1, Book b2);
+        bool CompareBooks(Book b1, Book b2);
+        string DisplayBookDetails(Book b);
+    }
+
+    class Book : IPages, IBookOperations
+    {
+        public string Author { get; }
+        public string Title { get; }
+        public string Publisher { get; }
+        public int Year { get; }
+
+        public Book(string author, string title, string publisher, int year)
+        {
+            Author = author;
+            Title = title;
+            Publisher = publisher;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"{Author}. {Title}. {Publisher}, {Year}.";
+        }
+
+        public static bool operator ==(Book b1, Book b2)
+        {
+            if (Object.ReferenceEquals(b1, b2))
+                return true;
+
+            if (b1 is null || b2 is null)
+                return false;
+
+            return b1.Author == b2.Author && b1.Title == b2.Title &&
+                b1.Publisher == b2.Publisher && b1.Year == b2.Year;
+        }
+
+        public static bool operator !=(Book b1, Book b2)
+        {
+            return !(b1 == b2);
+        }
+
+        public static bool operator >(Book b1, Book b2)
+        {
+            return b1.Volume() > b2.Volume();
+        }
+
+        public static bool operator <(Book b1, Book b2)
+        {
+            return b1.Volume() < b2.Volume();
+        }
+
+        public Book AddBooks(Book b)
+        {
+            if (Author != b.Author)
+                throw new ArgumentException("Cannot add books by different authors.");
+
+            if (Title != b.Title)
+                throw new ArgumentException("Cannot add books with different titles.");
+
+            if (Publisher != b.Publisher)
+                throw new ArgumentException("Cannot add books published by different publishers.");
+
+            if (Year != b.Year)
+                throw new ArgumentException("Cannot add books published in different years.");
+
+            return new Book(Author, Title, Publisher, Year) { Pages = Pages + b.Pages };
+        }
+
+        public bool CheckEquality(Book b1, Book b2)
+        {
+            return b1 == b2;
+        }
+
+        public bool CompareBooks(Book b1, Book b2)
+        {
+            return b1 > b2;
+        }
+
+        public string DisplayBookDetails(Book b)
+        {
+            return b.ToString();
+        }
+
+        public virtual int Pages { get; set; }
+
+        public virtual int Volume()
+        {
+            return Pages / 32;
+        }
+    }
+
+    class Monograph : Book
+    {
+        public int Illustrations { get; }
+
+        public Monograph(string author, string title, string publisher, int year, int pages, int illustrations) : base(author, title, publisher, year)
+        {
+            Pages = pages;
+            Illustrations = illustrations;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} {Pages} pages, {Illustrations} illustrations.";
+        }
+
+        public override int Volume()
+        {
+            return base.Volume() + Illustrations;
+        }
+    }
+
+    class MultiVolumeEdition : Book
+    {
+        public int Volumes { get; }
+        public int PagesPerVolume { get; set; }
+
+        public MultiVolumeEdition(string author, string title, string publisher, int year, int volumes, int pagesPerVolume) : base(author, title, publisher, year)
+        {
+            Volumes = volumes;
+            PagesPerVolume = pagesPerVolume;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} {Volumes} volumes, {PagesPerVolume} pages per volume.";
+        }
+
+        public override int Pages
+        {
+            get { return Volumes * PagesPerVolume; }
+            set { PagesPerVolume = value / Volumes; }
+        }
+    }
+}
---
+++ LAB3/Lab3/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace BookStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = new Book[]
+            {
+                new Book("Stewen Prata", "C++", "Addison-Wesley", 2011) { Pages = 480 },
+                new Book("Stewen Prata", "Unix", "Addison-Wesley", 2008) { Pages = 520 },
+                new Book("Al Sweigart", "Python", "Addison-Wesley", 2017) { Pages = 376 },
+                new Monograph("Hryhorii Starykiv", "Thoughts and notes", "Unknown Press", 2013, 1550, 15),
+                new MultiVolumeEdition("Uzbek", "Encyclopedia of Uzbekistan", "Uzbekistan Science", 2005, 12, 1440)
+            };
+
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Enter information about a new book:");
+            Console.Write("Author: ");
+            string author = Console.ReadLine();
+            Console.Write("Title: ");
+            string title = Console.ReadLine();
+            Console.Write("Publisher: ");
+            string publisher = Console.ReadLine();
+            Console.Write("Year: ");
+            int year = int.Parse(Console.ReadLine());
+            Console.Write("Pages: ");
+            int pages = int.Parse(Console.ReadLine());
+            Console.Write("Illustrations (0 if none): ");
+            int illustrations = int.Parse(Console.ReadLine());
+
+            Book newBook;
+            if (illustrations == 0)
+            {
+                newBook = new Book(author, title, publisher, year) { Pages = pages };
+            }
+            else
+            {
+                newBook = new Monograph(author, title, publisher, year, pages, illustrations);
+            }
+
+            Array.Resize(ref books, books.Length + 1);
+            books[books.Length - 1] = newBook;
+
+            Console.WriteLine("\nAll books:");
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+
+            Console.WriteLine();
+
+            // найбільша книжка
+            Book largest = books[0];
+            foreach (Book book in books)
+            {
+                if (book > largest)
+                {
+                    largest = book;
+                }
+            }
+
+            Console.WriteLine($"The largest book is: {largest} ({largest.Volume()} pages).");
+
+            if (largest is MultiVolumeEdition)
+            {
+                Console.Write
+                    ("It is a multivolume edition.\n\n");
+            }
+
+            // створення колекції книг певного автора і вивід
+            Console.WriteLine("Search books by specific author:");
+            Console.Write("Enter the author's name: ");
+            string searchAuthor = Console.ReadLine();
+
+            Book[] authorBooks = Array.FindAll(books, book => book.Author == searchAuthor);
+
+            Console.WriteLine($"Books by {searchAuthor}:");
+            foreach (Book book in authorBooks)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
