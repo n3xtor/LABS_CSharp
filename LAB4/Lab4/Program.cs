2abdd613526f2863166217449e73807765e9e954
Pavlo Kikis <99750479+n3xtor@users.noreply.github.com>
2023-05-10 12:22:21 +0300

    LAB4

---
+++ LAB4/Lab4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        // Метод-обробник події
+        static public void OnPageDamage(object sender, PageDamageEventArgs e)
+        {
+            Console.WriteLine($"Page {e.PageNumber} of {((Book)sender).Title} was damaged ({e.DamageType}).\n");
+        }
+        static void Main(string[] args)
+        {
+            Book book = new Book("John Smith", "PoorBook", "Addison-Wesley", 2007);
+            book.PageDamage += OnPageDamage;
+
+            // Якщо хтось зігнув сторінку 10
+            book.DamagePage(10, "page was folded");
+
+            // Якщо хтось написав текст на сторінці 15
+            book.DamagePage(15, "text was written on the page");
+
+            // Якщо хтось вирвав сторінку 5
+            book.DamagePage(5, "page was torn out");
+        }
+    }
+}
