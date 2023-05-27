c14982d3e6f4ee01aa2b733d5687e309ecfaba4e
Pavlo Kikis <99750479+n3xtor@users.noreply.github.com>
2023-05-27 14:59:42 +0300

    Game

---
+++ Game/Game/MainForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace BaloonGame
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public void PenaltyTimer_Tick(object sender, EventArgs e)
+        {
+            penalty++;
+            penaltyLabel.Text = $"Penalty: {penalty}";
+
+            if (penalty >= 10) // Якщо штраф досягне 10, гра завершується
+            {
+                EndGame();
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.penaltyLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(12, 9);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(68, 20);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "Score: 0";
+            // 
+            // penaltyLabel
+            // 
+            this.penaltyLabel.AutoSize = true;
+            this.penaltyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.penaltyLabel.Location = new System.Drawing.Point(12, 39);
+            this.penaltyLabel.Name = "penaltyLabel";
+            this.penaltyLabel.Size = new System.Drawing.Size(78, 20);
+            this.penaltyLabel.TabIndex = 1;
+            this.penaltyLabel.Text = "Penalty: 0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(399, 376);
+            this.Controls.Add(this.penaltyLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "PopTheBall";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label penaltyLabel;
+    }
+}
---
+++ Game/Game/MainForm.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BaloonGame
+{
+    public partial class MainForm : Form
+    {
+        private int score;
+        private int penalty;
+        private Random random;
+        private Timer timer;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            score = 0;
+            penalty = 0;
+            random = new Random();
+            timer = new Timer();
+            timer.Interval = 700; // Інтервал в мілісекундах
+            timer.Tick += Timer_Tick;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            GenerateBall();
+        }
+
+        private void GenerateBall()
+        {
+            int diameter = random.Next(30, 100); // Випадковий діаметр кульки
+            int x = random.Next(0, ClientSize.Width - diameter); // Випадкове положення по осі X
+            int y = random.Next(0, ClientSize.Height - diameter); // Випадкове положення по осі Y
+
+            Ball ball = new Ball(x, y, diameter, this); // Передаємо поточний об'єкт MainForm як аргумент
+            ball.Click += Ball_Click;
+            Controls.Add(ball);
+
+            Color[] colors = { Color.Red, Color.Blue, Color.Green, Color.Yellow };
+            Color color = colors[random.Next(colors.Length)];
+            ball.BackColor = color;
+
+            ball.StartAnimation();
+        }
+
+        private void Ball_Click(object sender, EventArgs e)
+        {
+            Ball ball = (Ball)sender;
+            ball.StopAnimation();
+            Controls.Remove(ball);
+
+            score++;
+            scoreLabel.Text = $"Score: {score}";
+        }
+
+        private void penaltyTimer_Tick(object sender, EventArgs e)
+        {
+            penalty++;
+            penaltyLabel.Text = $"Penalty: {penalty}";
+
+            if (penalty >= 10) // Якщо штраф досягне 10, гра завершується
+            {
+                EndGame();
+            }
+        }
+
+        private void EndGame()
+        {
+            timer.Stop();
+            MessageBox.Show($"Game Over! Your score: {score}");
+            Close();
+        }
+    }
+
+    public class Ball : PictureBox
+{
+    private Timer animationTimer;
+    private int animationDuration;
+    private MainForm parentForm;
+
+    public Ball(int x, int y, int diameter, MainForm parentForm)
+    {
+        Location = new Point(x, y);
+        Size = new Size(diameter, diameter);
+        BackColor = Color.Red;
+        BorderStyle = BorderStyle.FixedSingle;
+        animationDuration = 700; // Тривалість анімації кульки. Змініть за потреби.
+        animationTimer = new Timer();
+        animationTimer.Interval = animationDuration;
+        animationTimer.Tick += AnimationTimer_Tick;
+        this.parentForm = parentForm;
+    }
+
+    public void StartAnimation()
+    {
+        animationTimer.Start();
+    }
+
+    public void StopAnimation()
+    {
+        animationTimer.Stop();
+    }
+
+    private void AnimationTimer_Tick(object sender, EventArgs e)
+    {
+        StopAnimation();
+        parentForm.Invoke((MethodInvoker)delegate
+        {
+            parentForm.PenaltyTimer_Tick(null, null);
+            parentForm.Controls.Remove(this);
+        });
+    }
+}
+}
